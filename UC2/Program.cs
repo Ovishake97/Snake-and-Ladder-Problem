fccd2c73faa1f48072d4ec851acc14b9ce695cad
Abhishek Kumar Sharma <theahishek.aks22@gmail.com>
2020-10-04 15:07:40 +0530

    added to the UC1 and 2 branch

---
+++ UC2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Welcome to Snakes and Ladders Problem");
+            int position = 0;
+
+            Random r = new Random();
+            int diceMove = r.Next(1, 7);
+            Console.WriteLine("Obtained number from the dice: {0}", +diceMove);
+
+        }
+    }
+}
