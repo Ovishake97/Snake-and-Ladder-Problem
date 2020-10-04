0fccc3e20904c287fadde11680f59f024b4cd276
Abhishek Kumar Sharma <theahishek.aks22@gmail.com>
2020-10-04 15:12:32 +0530

    Added

---
+++ UC4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initiaalisation and welcome message  
+            Console.WriteLine("Welcome to Snakes and Ladders Problem");
+            int position = 0;
+
+            Random r = new Random();
+
+            String[] boardMoves = { "No Play", "Snake", "Ladder" };
+            Random rand = new Random();
+
+            while (position < 100)
+
+            {   //Assigning die moves and player positions
+                int diceMove = r.Next(1, 7);
+                int index = rand.Next(boardMoves.Length);
+                switch (index)
+                {
+                    //In case of snake
+                    case 1:
+                        position = position - diceMove;
+                        break;
+                        //In case of ladder
+                    case 2:
+                        position = position + diceMove;
+                        break;
+                        //In case of no play
+                    default: break;
+                }
+                if (position < 0)
+                {
+                    position = 0;
+                }
+                //Printing positions
+                Console.WriteLine("Your new position: {0}", +position);
+            }
+
+        }
+    }
+}
